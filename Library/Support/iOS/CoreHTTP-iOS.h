68b364215ca294724748939111c8abd2f280ac2b
Saul Mora <saul.mora@liulishuo.com>
2016-08-18 19:16:23 +0800

    Adding Mac target

---
+++ Library/Support/iOS/CoreHTTP-iOS.h
@@ -0,0 +1,19 @@
+//
+//  CoreHTTP.h
+//  CoreHTTP
+//
+//  Created by Saul Mora on 6/28/16.
+//  Copyright © 2016 Magical Panda Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreHTTP.
+FOUNDATION_EXPORT double CoreHTTPVersionNumber;
+
+//! Project version string for CoreHTTP.
+FOUNDATION_EXPORT const unsigned char CoreHTTPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreHTTP/PublicHeader.h>
+
+
