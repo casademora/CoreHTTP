c625a03c4107da499a4b76ef38b1e013ad49d1cb
Saul Mora <saul@casademora.com>
2016-10-28 20:25:59 +0800

    Use NASA Demo API Key

--- Sample/NASA Photo of the Day/NASA Photo of the Day/AppDelegate.swift
+++ Sample/NASA Photo of the Day/NASA Photo of the Day/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
   {
-    register(host: NASAAPODHost(apiKey: "insert your API key here"))
+    register(host: NASAAPODHost(apiKey: "NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo"))
     return true
   }
 }
