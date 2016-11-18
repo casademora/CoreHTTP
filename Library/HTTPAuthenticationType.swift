1235be229916ea62fc860f29eb180f865562fb41
Saul Mora <saul@casademora.com>
2016-11-18 15:59:17 +0800

    Extract AuthenticationType into seperate file

---
+++ Library/HTTPAuthenticationType.swift
@@ -0,0 +1,78 @@
+//
+//  HTTPAuthenticationType.swift
+//  CoreHTTP
+//
+//  Created by Saul Mora on 11/18/16.
+//  Copyright © 2016 Magical Panda Software. All rights reserved.
+//
+
+import Foundation
+
+public typealias AuthenticationToken = String
+public enum HTTPAuthenticationType
+{
+  case none
+  case queryParameters(String, AuthenticationToken)
+  case basic(AuthenticationToken)
+  case oauth(AuthenticationToken)
+  
+  var authenticationHeaderValue: String
+  {
+    switch self
+    {
+    case .basic(let token):
+      return "Basic \(token)"
+    case .oauth(let token):
+      return "Bearer \(token)"
+    default:
+      return ""
+    }
+  }
+  
+  func authenticate(request: URLRequest) -> URLRequest
+  {
+    func authentication() -> (URLRequest) -> URLRequest
+    {
+      switch self
+      {
+      case .queryParameters:
+        return authenticateQuery
+      case .basic(_), .oauth(_):
+        return authenticateHeader
+      case .none:
+        return noAuthentication
+      }
+    }
+    let function = authentication()
+    return function(request)
+  }
+  
+  private func noAuthentication(request: URLRequest) -> URLRequest
+  {
+    return request
+  }
+  
+  private func authenticateHeader(request: URLRequest) -> URLRequest
+  {
+    var authenticatedRequest = request
+    authenticatedRequest.setValue(authenticationHeaderValue, forHTTPHeaderField: "Authorization")
+    return authenticatedRequest
+  }
+  
+  private func authenticateQuery(request: URLRequest) -> URLRequest
+  {
+    guard
+      let url = request.url,
+      var components = URLComponents(url: url, resolvingAgainstBaseURL: false),
+      case let .queryParameters(key, value) = self
+      else { return request }
+    
+    var queryItems = components.queryItems ?? []
+    queryItems.append(URLQueryItem(name: key, value: value))
+    components.queryItems = queryItems
+    
+    var authenticatedRequest = request
+    authenticatedRequest.url = components.url
+    return authenticatedRequest
+  }
+}
--- Library/HTTPHost.swift
+++ Library/HTTPHost.swift
@@ -11,77 +11,7 @@ import Result
 public typealias ResponseValidationFunction = (Data?) -> (HTTPURLResponse) -> Result<Data, HTTPResponseError>
 public typealias AuthenticateURLRequestFunction = (_ request: URLRequest) -> (URLRequest)
 public typealias PreprocessRequestFunction = (URLRequest) -> URLRequest
-
-public typealias AuthenticationToken = String
-public enum AuthenticationType
-{
-  case none
-  case queryParameters(String, AuthenticationToken)
-  case basic(AuthenticationToken)
-  case oauth(AuthenticationToken)
-  
-  var authenticationHeaderValue: String
-  {
-    switch self
-    {
-      case .basic(let token):
-        return "Basic \(token)"
-      case .oauth(let token):
-        return "Bearer \(token)"
-      default:
-        return ""
-    }
-  }
-
-  func authenticate(request: URLRequest) -> URLRequest
-  {
-    func authentication() -> (URLRequest) -> URLRequest
-    {
-      switch self
-      {
-        case .queryParameters:
-          return authenticateQuery
-        case .basic(_), .oauth(_):
-          return authenticateHeader
-        case .none:
-          return noAuthentication
-      }
-    }
-    let function = authentication()
-    return function(request)
-  }
-  
-  private func noAuthentication(request: URLRequest) -> URLRequest
-  {
-    return request
-  }
-  
-  private func authenticateHeader(request: URLRequest) -> URLRequest
-  {
-    var authenticatedRequest = request
-    authenticatedRequest.setValue(authenticationHeaderValue, forHTTPHeaderField: "Authorization")
-    return authenticatedRequest
-  }
-
-  private func authenticateQuery(request: URLRequest) -> URLRequest
-  {
-    guard
-      let url = request.url,
-      var components = URLComponents(url: url, resolvingAgainstBaseURL: false),
-      case let .queryParameters(key, value) = self
-    else { return request }
-    
-    var queryItems = components.queryItems ?? []
-    queryItems.append(URLQueryItem(name: key, value: value))
-    components.queryItems = queryItems
-    
-    var authenticatedRequest = request
-    authenticatedRequest.url = components.url
-    return authenticatedRequest
-  }
-}
-
-public typealias GenerateAuthenticationCredentialsFunction = (Void) -> (AuthenticationType)
+public typealias GenerateAuthenticationCredentialsFunction = (Void) -> (HTTPAuthenticationType)
 
 public protocol HTTPHostProtocol: Hashable
 {
@@ -168,7 +98,7 @@ fileprivate func defaultValidation(data: Data?) -> (HTTPURLResponse) -> Result<D
   }
 }
 
-fileprivate func defaultAuthentication() -> AuthenticationType
+fileprivate func defaultAuthentication() -> HTTPAuthenticationType
 {
   return .none
 }
